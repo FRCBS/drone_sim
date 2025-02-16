0eb967d0c295d7f8dae08efbfeac42590e991221
erastop1 <panu.erasto@aalto.fi>
2025-02-16 21:28:42 +0200

    Update simulointi_s100_d50_github.R

--- src/simulointi_s100_d50_github.R
+++ src/simulointi_s100_d50_github.R
@@ -1,57 +1,62 @@
 
-# Alustukset
+# Initializations
 rm(list = ls())
 start.time <- Sys.time()
 
-# Lue traumaennusteet tiedostosta ja alusta simulointia varten
+# Read the trauma intensities from file and initialize
 ##############################################################
-setwd("~/Documents/Kokoveri")
-ennusteet=read.csv(file = "Trauma_ennusteet_1x1.csv",header = TRUE,sep=";",dec=",")
-ennusteet$skaalatut=ennusteet$trauma_saatto_enn/(365*24*60)       # Muunnetaan tapahtuma-lukumääriksi minuutissa
-ennusteet$skaalatut[ennusteet$skaalatut==0]=1.119e-09/(365*24*60)  # Ei sallita nollia, mutta laitetaan joku tosi pieni
-intensities <- ennusteet$skaalatut
+predictions=read.csv(file = "../data/trauma_prob_1x1.csv",header = TRUE,sep=";",dec=",")
 
-# Lue karttaruutujen etäisyystiedot (ovat sisällä matriisissa)
-load('Etäisyydet/et_mat.RData')
+# Transform the number of events per year to number of events per minute
+predictions$scaled=predictions$trauma_saatto_enn/(365*24*60)       
+
+# For numerical reasons, the change the zero into a very small positive value 
+predictions$scaled[predictions$scaled==0]=1.119e-09/(365*24*60) 
+intensities <- predictions$scaled
+
+# Read the grid distances from potential BDC's
+load('../data/et_mat.RData')
 
 
 
-# Simuloinnin parametrit
+# Set the simulation parameters
 ##################################################################
-sim_time <- 365*24*60         # Simulaation aikakehys minuutteina
-speed=100                     # Nopeus
-Dis_limit=50                  # Maksimikantavuus yhteen suuntaan
-N_drone=1                     # Droneja per base
-N_base=ncol(et_mat)           # Beissien lkm
-zero_vec=rep(0,N_base)        # Apuvektori nollia        
+sim_time <- 365*24*60         # Simulation time frame
+speed=100                     # Drone speed in km/h
+Dis_limit=50                  # Max range to _one_ direction (the overall range is double)
+N_drone=1                     # Drones per BDC
+N_base=ncol(et_mat)           # Number of BDC's
+N=10                          # Size of simulations 
+
+zero_vec=rep(0,N_base)        # Vector of zeros         
 set.seed(123)                 # Set seed for reproducibility
-N=10                        # Simuloinnin koko
 
 
-# Funktion määrittely tapahtuma-aikojen generointiin
+
+# Define function to generate event times
 ##################################################################
 generate_events <- function(intensities, sim_time) {
   num_points=length(intensities)
   events_list <- vector('list', num_points)
   
-  # Luodaan tapahtumat jokaiselle karttaruudulle erikseen
+  # Create events to each map grid separately
   for (i in 1:num_points) {
     time <- 0
     events <- c()
     intensity <- intensities[i]
     
     while (time < sim_time) {
-      # Generoidaan seuraavan tapahtuman aika eksponenttijakauman avulla
+      # Generate the next event time 
       interarrival_time <- rexp(1, rate = intensity)
       
-      # Lisätään karttaruudun i tapahtuma listaan
+      # Add the event to the list of event times of map grid i
       events <- c(events, time + interarrival_time)
       
-      # Päivitetään aikaa seuraavaan tapahtumaan
+      # Update the time variable
       time <- time + interarrival_time
     }
     
-    # Talletetaan listaan lokaatio ja sitä vastaavat tapahtumien ajanhetket
+    # Store the events of location i
     events_list[[i]] <- list(events=events, location=i)
   }
   return(events_list)
@@ -60,95 +65,90 @@ generate_events <- function(intensities, sim_time) {
 
 
 #############################################################################
-# Tästä alkaa makrotason koodi
+# The macro level code starts here
 ##############################################################################
 
 
 result_list <- vector('list', N)
 for (k in 1:N){
-  print(k)
   
-  # Generoidaan tapahtumat aiemmin määritellyllä funktiolla
+  print(k) 
+  
+  # Generate the events using the function defined above
   events_list <- generate_events(intensities, sim_time)
   
-  # Irrotetaan listasta event_list tapahtuma ajat ja lokaatiot matriisiin event_matrix
+  # Distill and reframe the events and location into matrix 'event_matrix'
   event_times <- unlist(sapply(events_list, function(x) x$events))
   locations <- rep(sapply(events_list, function(x) x$location), sapply(events_list, function(x) length(x$events)))
   event_matrix <- rbind(event_times, locations)
   
-  # Järjestä tapahtumat matriisissa kasvavaan aikajärjestykseen
-  sorted_indices <- order(event_matrix[1, ]) # Get the indices that sort the event times
+  # Sort the generated events into increasing order by time 
+  sorted_indices <- order(event_matrix[1, ])            # Get the indices that sort the event times
   sorted_event_times <- event_matrix[1, sorted_indices] # Sort event times
-  sorted_locations <- event_matrix[2, sorted_indices] # Sort corresponding locations
+  sorted_locations <- event_matrix[2, sorted_indices]   # Sort corresponding locations
   sorted_event_matrix_temp <- rbind(sorted_event_times, sorted_locations)
   
-  # Ota pois ne ajanhetket, jotka menivät aikaikkunan ulkopuolelle
+  # Take out the events that are outside of the simulation time frame
   sorted_event_matrix <- sorted_event_matrix_temp[,sorted_event_matrix_temp[1,]<=sim_time]
   
-  # Määritellään kierroskohtaisia muuttujia
-  N_ev=ncol(sorted_event_matrix)  # Tapahtumien lukumäärä tällä kierroksella
-  End_times=rep(0,N_base)         # Jokaisen beissin edeltävän työn lopetusaika (minuutteina)
-  wait_time_store=rep(-1,N_ev)    # Odotusaika jokaisen tapahtuman valmistumiseen 
-  # ja jollei saada hoidettua, niin wait_time_store =-1
-  queue_time_store=rep(-1,N_ev)   # Jonotusaika jokaisen tapahtuman valmistumiseen 
-  # ja jollei saada hoidettua, niin queue_time_store =-1
-  base_store=rep(-1,N_ev)         # Apumuuttuja, joka tallettaa tapahtuman hoitavan basen    
-  # ja jollei saada hoidettua, niin base_store =-1
+  # Define some additional variables 
+  N_ev=ncol(sorted_event_matrix)  # Number of events within this simulation round
+  End_times=rep(0,N_base)         # Initialize the end times 
   
-  # Käy läpi jokainen event-ajankohta
+  # Initialize saved variables for this simulation round 
+  wait_time_store=rep(-1,N_ev)    # Variable describing the _wait time_ of each event
+                                  # ... which gets value -1 in case the event cannot be handled) 
+  queue_time_store=rep(-1,N_ev)   # Variable describing the _queue time_ of each event 
+                                  # ... which gets value -1 in case the event cannot be handled) 
+  base_store=rep(-1,N_ev)         # Variable storing the base (BCD) of each event     
+                                  # ... which gets value -1 in case the event cannot be handled) 
+  
+  # Go through the events for this simulation round
   for (i in 1:N_ev) {
     
-    # Selvitetään tapahtuma-ajankohdan i lokaatioINDEKSI 
+    # Check the location index of the event i 
     location_index=sorted_event_matrix[2,i] 
-    # location=ennusteet$nro[location_index]
+  
+    # Calculate different time variables for this event 
+    travel_times=et_mat[location_index,]/speed*60                    # Travel (flight) times in minutes
+    queue_times=pmax(zero_vec,(End_times-sorted_event_matrix[1,i]))  # Potential queue times (before the previous is finished)  
+    wait_times=travel_times+queue_times                              # Waiting time= travel time + queue time
     
-    # Määritä valmistumisajat yhteen suuntaan kulkien jokaisesta beissistä
-    travel_times=et_mat[location_index,]/speed*60                    # Matka-ajat=matka/nopeus
-    queue_times=pmax(zero_vec,(End_times-sorted_event_matrix[1,i]))  # Jonotusajat edellisen valmistumisajankohdista 
-    wait_times=travel_times+queue_times                              # Valmistumisajat = matka-ajat + jonotusajat
-    
-    # Haetaan ne baset, jotka alle kriittisen etäisyyden
+    # Find the bases (BDC's) that are within the oneway range
     pot_base_ind <- which(et_mat[location_index,] < Dis_limit)  
     
-    # Jos/kun tälläisiä baseja löytyy, niin...
+    # And if there are such..
     if (length(pot_base_ind)>0){
       
-      # .. hae (=se base), joka olisi ensimmäisenä perillä 
+      # .. then select the base that has the fastest delivery time 
       shortest_index=which.min(wait_times)
-      #print(c(i, shortest_index))
-      
-      # Lasketaan matkaan kuluva aika nopeimmasta 
-      travel_time=(et_mat[location_index, shortest_index]/speed)*60
       travel_time=travel_times[shortest_index]
-      
       queue_time=queue_times[shortest_index]
 
-      # Määritellään valitun basen seuraava valmistumisaika
-      # Tässä matka-aika on kahteen kertaan (edestakaisin)
+      # Calculate the finish time of the event (drone is back again)
+      # as the event time + possible queue time + flight time (return)
       End_times[shortest_index]=sorted_event_matrix[1,i]+queue_time+2*travel_time  
       
-      wait_time_store[i]=queue_time+travel_time # Kokonaisodotusaika talteen
-      queue_time_store[i]=queue_time            # Jonotusaika talteen
-      base_store[i]=shortest_index              # Base talteen
-    } # jos löytyy baseja alle kriittisen etäisyyden
-  } # for-luuppi yli tapahtumien
+      wait_time_store[i]=queue_time+travel_time # Store the wait time (queue + flight)
+      queue_time_store[i]=queue_time            # Store the queue time
+      base_store[i]=shortest_index              # Store the base
+    } #  if (length(pot_base_ind)>0)
+  } #  for (i in 1:N_ev)
   
+
   
-  print(sum(queue_time_store!=-1)/length(queue_time_store)*100)
-  print(sum(wait_time_store!=-1)/length(queue_time_store)*100)
-  print(sum(base_store!=-1)/length(queue_time_store)*100)
+  # Store the variables for this simulation round into a list:
+  # -wait_times: waiting times describing the time how long a patient need to wait before service
+  # -queue_times: how long does it take to wait until the drone is available from the previous service
+  # -locations: location indices of the events
+  # -bases: base (BDC) indices that provides the fastest service
+  # NOTE: value is -1 is stored if the drone is out of reach (except for locations)
   
-  # Talletukset tälle kierrokselle
-  result_list[[k]] <- list(wait_times=wait_time_store, queue_times=queue_time_store, locations=sorted_event_matrix[2,],bases=base_store )
+  result_list[[k]] <- list(wait_times=wait_time_store, queue_times=queue_time_store, locations=sorted_event_matrix[2,], bases=base_store)
 }
 
-# Listan jokaiseen elementtiin k talletaaan seuraavat suureet:
-# -wait_times: odotusajat eli kuinka kauan potilas joutuu odottamaan
-# -queue_times: jonotusaika dronelle eli kuinka kauan drone joutuisi odottamaan edellisen dronen paluuta siinä nopeimmassa vaihtoehdossa
-# -locations: tapahtuman karttakoordinaatti
-# -bases: ko. tapahtumaa vastaava base, josta saadaan nopein kuljetus
 
-# Kaikissa paitsi locations-muuttujassa arvona -1 mikäli tapahtumaa ei saada hoidettua kantavuuden rajoissa
+
 
 
 
